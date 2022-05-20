abd877a4645dd7e1e1fc34c0aaa419b9d44ee08e
oliver zulett <oliverzulett@gmail.com>
2022-05-19 22:53:42 -0400

    ui modifications

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -2,8 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { map, Observable, Subscription } from 'rxjs';
 import { CharactersService } from './characters.service';
 import { Character } from './character.interface';
-import { Store } from '@ngrx/store';
-import { addCharacters } from '../../ngrx/actions/character.actions';
 
 export interface Info {
   count: number,
@@ -20,38 +18,18 @@ export interface Info {
 export class CharactersComponent implements OnInit, OnDestroy {
   characters$?: Observable<Character[]>;
 
-  private info?: Info;
   private subscriptions = new Subscription();
 
   constructor(
     private charactersService: CharactersService,
-    private store: Store<{ characters: Character[] }>
-  ) {
-    this.characters$ = store.select('characters');
-    // this.characters$ = this.getCharacters();
-  }
+  ) {}
   
   ngOnInit(): void {
-    this.characters$ = this.getCharacters();
+    this.characters$ = this.charactersService.getCharacters();
     
   }
 
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
-  }
-
-  setCharacters() {
-    // TODO: Dispatch an increment action
-  }
-
-  private getCharacters() {
-    return this.charactersService.getCharacters()
-    .pipe(
-      map((apiResponse: any) => {
-        console.log(apiResponse);
-        this.store.dispatch(addCharacters(apiResponse.results))
-        return apiResponse.results;
-      })
-    )
   }
 }
--- src/app/pages/characters/characters.service.ts
+++ src/app/pages/characters/characters.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
-import { RickAndMortyApiService } from '../../services/rick-and-morty-api.service';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Injectable()
 export class CharactersService {
-  private API_URL = 'https://rickandmortyapi.com/api/character'
+  private API_URL = 'https://rickandmortyapi.com/api/character';
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  constructor(private httpClient: HttpClient) {}
 
   getCharacters() {
-    return this.httpClient.get(this.API_URL);
+    return this.httpClient.get(this.API_URL).pipe(
+      map((apiResponse: any) => {
+        return apiResponse.results;
+      })
+    );
   }
 }
